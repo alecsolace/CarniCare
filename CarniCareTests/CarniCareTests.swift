d31a4e27844fe3ae90b5632d68e00ca7da534957
Alexander Aguirre <keevinaguirre@gmail.com>
2025-05-09 13:26:55 +0200

    Initial Commit

---
+++ CarniCareTests/CarniCareTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarniCareTests.swift
+//  CarniCareTests
+//
+//  Created by Alexander Aguirre on 9/5/25.
+//
+
+import Testing
+@testable import CarniCare
+
+struct CarniCareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
